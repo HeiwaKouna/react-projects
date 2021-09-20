631661c8ac93a711cebfebe5c1da3ab2e5737483
Kouna <heiwakouna@gmail.com>
2021-09-20 16:40:50 +0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+        perferendis doloremque id excepturi explicabo quidem, nesciunt culpa
+        delectus vel iure doloribus, eos nemo quasi, possimus recusandae cumque!
+        Porro, aut. Iure consequuntur similique voluptatum nemo quaerat mollitia
+        quam voluptas velit corporis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
